cebed40644f38a95bb06af41ca67fb50bf68a226
Enzo Degraeve <enzodeg40@gmail.com>
2022-06-08 23:14:51 +0200

    Création page USK

--- usk-old.php
+++ usk-old.php
@@ -4,35 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>USK</title>
-    <link rel="stylesheet" href="css/sidenav.css">
-    <link rel="stylesheet" href="css/usk.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/usk-old.css">
     
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400&display=swap" rel="stylesheet">
 </head>
 <body>
-    <?php 
-        // Lit et affiche le contenu du fichier 
-        $sidenav = file_get_contents('assets/html/sidenav.html');
-        echo $sidenav;
-    ?>
-
-    <div class="top">
-        <img class="btnOpenSideBar" src="assets/icons/menu.svg" onclick="openNav()" alt="">
-    </div>
-
+    <div class="title">BIOGRAPHIES DES URBAN SKETCHERS</div>
     <center>
-        <img class="logo" src="assets/images/usk.png" alt="">
-    </center>
+        <div class="list">
 
-    <div class="list">
-        <div class="item">
+
+
+            <div class="item">
                 <div class="username">Marion Rivolier</div>
-                <a class="youtube-link" href="https://youtu.be/eP0h8bhy8Hs">
-                    <img class="avatar" src="assets/images/usk/marion_rivolier.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/marion_rivolier.jpg" alt="">
                 <div class="description">
                     Marion Rivolier est peintre et scénographe. Partout où elle va, elle prend carnet, pinceaux et aquarelles dans son sac.
                     Ce qui l’intéresse le plus est de saisir au pinceau le mouvement et la lumière sur le vif. Elle est membre active des
@@ -42,15 +30,15 @@
                 </div>
                 <div class="links">
                     <a href="https://www.instagram.com/marion_rivolier/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>
 
                     <a href="http://marionrivolier.blogspot.fr/">
-                        <div class="link-item"><img src="assets/icons/blogspot.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/blogspot.ico" alt=""></div>
                     </a>
 
                     <a href="http://marionrivolier.fr/">
-                        <div class="link-item"><img src="assets/icons/link.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/link.ico" alt=""></div>
                     </a>                
                 </div>
             </div>
@@ -60,13 +48,11 @@
 
             <div class="item">
                 <div class="username">Stéphanie Senez</div>
-                <a class="youtube-link" href="https://youtu.be/XOigLklBpy4">
-                    <img class="avatar" src="assets/images/usk/stephanie_senez.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/stephanie_senez.jpg" alt="">
                 <div class="description">Intéressée par les arts depuis toujours, Stéphanie pratique la sculpture, la photographie et le dessin. En 2017, elle découvre la communauté Urbansketchers et depuis, son stylo et son aquarelle ne la quittent plus, elle ose dessiner in situ. Sa ligne se délie au fil des expositions, des rencontres. Cette ligne prend vie, elle suggère sans s’interrompre. Elle aime composer, trouver un équilibre, une harmonie. Mais ce qui l’intéresse le plus c’est l’instant, les rencontres, les moments partagés à travers le dessin.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/lalignecontinue/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>           
                 </div>
             </div>
@@ -76,13 +62,11 @@
 
             <div class="item">
                 <div class="username">Carnets d’Agnès</div>
-                <a class="youtube-link" href="https://youtu.be/5zTAXVsLJDo">
-                    <img class="avatar" src="assets/images/usk/agnes_goyet.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/agnes_goyet.jpg" alt="">
                 <div class="description">Agnès découvre le bonheur de dessiner dehors lorsqu’elle devient parisienne. Paris est son sujet de prédilection, elle aime s’installer aux terrasses des bistrots, se laisser imprégner des ambiances comme pour arrêter le temps. Elle transporte ses carnets partout, au coin de sa rue ou au bout du monde. Elle a choisi l’aquarelle, technique légère et spontanée qui s’adapte parfaitement aux carnets de voyage. Croquer sur le vif est pour elle le meilleur moyen d’être dans la vraie vie et toujours l’occasion de belles surprises. Elle est membre et correspondante des Urban Sketchers parisiens depuis 2018.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/carnets_d_agnes/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>            
                 </div>
             </div>
@@ -91,21 +75,19 @@
 
             <div class="item">
                 <div class="username">Claire Archenault</div>
-                <a class="youtube-link" href="https://youtu.be/bk2cIHnuLas">
-                    <img class="avatar" src="assets/images/usk/claire_archenault.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/claire_archenault.jpg" alt="">
                 <div class="description">Claire Archenault est peintre et graveur et réalise des carnets au fil des jours. Son parcours professionnel a été costumière et décoratrice dans le théâtre et le music-hall, roughman pour la pub, puis peintre en décors. Elle a aussi réalisé des copies d’oeuvres de maître au musée du Louvre. Si l’univers de l’estampe est devenu ces dernières années un mode d’expression et de transmission important, la peinture reste son langage favori, et l’aquarelle un médium de prédilection pour le travail en plein air. Elle est membre de plusieurs associations d’artistes depuis 20 ans et de UrbanSketchers Paris depuis 2015.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/clairearchenault/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>
 
                     <a href="http://clairearchenault.blogspot.com/">
-                        <div class="link-item"><img src="assets/icons/blogspot.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/blogspot.ico" alt=""></div>
                     </a>
 
                     <a href="https://www.facebook.com/claire.archenault">
-                        <div class="link-item"><img src="assets/icons/facebook.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/facebook.ico" alt=""></div>
                     </a>                
                 </div>
             </div>
@@ -114,21 +96,19 @@
 
             <div class="item">
                 <div class="username">Brigitte Lannaud</div>
-                <a class="youtube-link" href="https://youtu.be/BYrQsW2-yak">
-                    <img class="avatar" src="assets/images/usk/brigitte_lannaud.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/brigitte_lannaud.jpg" alt="">
                 <div class="description">Brigitte Lannaud est peintre et illustratrice pour la presse. Ancienne éditrice, elle concilie sa passion pour la littérature et la peinture dans des séries à l’aquarelle de portraits de livres de poche anciens #vintagebookproject et de librairies dont elle peint les façades à travers l’hexagone. Elle est membre et correspondante de Urban Sketchers Paris depuis 2015.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/blannaudlevy/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>
 
                     <a href="http://brigittelannaudlevy.blogspot.com/">
-                        <div class="link-item"><img src="assets/icons/blogspot.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/blogspot.ico" alt=""></div>
                     </a>
 
                     <a href="https://www.facebook.com/brigitte.lannaud.levy">
-                        <div class="link-item"><img src="assets/icons/facebook.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/facebook.ico" alt=""></div>
                     </a>                
                 </div>
             </div>
@@ -137,21 +117,19 @@
 
             <div class="item">
                 <div class="username">Mat Let</div>
-                <a class="youtube-link" href="https://youtu.be/ovxU654PetA">
-                    <img class="avatar" src="assets/images/usk/mat_let.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/mat_let.jpg" alt="">
                 <div class="description">Dessinateur autodidacte, Mat s’est lancé dans le dessin à plein temps en 2017. Grand voyageur, il a raconté dans ses carnets ses aventures en Iran, au Cameroun ou au Brésil avec humour et l’envie de se connecter aux gens et aux lieux qui croisent sa route. C’est cette même route qui l’a ramené chez lui, à Paris, où il a fini par prendre conscience que la magie du voyage n’a rien à voir avec la distance parcourue mais plutôt avec le regard que l’on porte sur ce qui nous entoure. On le retrouve donc dans la capitale, un crayon à la main, le carnet dans l’autre en train de croquer tel un reporter de la vie quotidienne dans le métro, dans les bars où dans les parcs, à l’affut de l’incongru, de l’imprévu et de l’humanité qui s’y déroule.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/matlet/">
-                        <div class="link-item"><img src="assets/icons/instagram.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>
 
                     <a href="http://www.matlet.fr/">
-                        <div class="link-item"><img src="assets/icons/link.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/link.ico" alt=""></div>
                     </a>
 
                     <a href="https://www.facebook.com/carnetsmatlet/">
-                        <div class="link-item"><img src="assets/icons/facebook.png" alt=""></div>
+                        <div class="link-item"><img src="assets/icons/facebook.ico" alt=""></div>
                     </a>                
                 </div>
             </div>
@@ -159,20 +137,25 @@
 
             <div class="item">
                 <div class="username">Tula Moraes</div>
-                <a class="youtube-link" href="https://youtu.be/c5h8qzo8keI">
-                    <img class="avatar" src="assets/images/usk/tula_moraes.jpg" alt="">
-                </a>
+                <img class="avatar" src="assets/images/usk/tula_moraes.jpg" alt="">
                 <div class="description">Partageant son temps entre Paris et Rio depuis sa plus tendre enfance, Tula a d’abord suivi le cours préparatoire des Beaux Arts de Versailles avant de s’envoler pour la Faculdade da Cidade à Rio de Janeiro en 1998 pour être formée au Design Graphique. Depuis 2011, Tula pratique le dessin chaque jour : explorant la ville et scrutant les médias, elle dessine, croque le réel, interroge son quotidien au gré des rencontres à Paris. C’est ainsi qu’elle nourrit sa créativité et prépare son travail de peintre dans son atelier.</div>
                 <div class="links">
                     <a href="https://www.instagram.com/tulamoraes/">
-                        <div class="link-item">
-                            <img src="assets/icons/instagram.png" alt="">
-                        </div>
+                        <div class="link-item"><img src="assets/icons/instagram.ico" alt=""></div>
                     </a>           
                 </div>
             </div>
-    </div>
 
-    <script src="js/sidebar.js"></script>
+
+
+
+
+
+
+
+
+
+        </div>
+    </center>
 </body>
 </html>
